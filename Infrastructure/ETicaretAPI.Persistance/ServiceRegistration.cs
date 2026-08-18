ca42ca7200300ceb7b2c166d7bff8b07c5fc35e9
Sercan Çintosun <sercancintosun000@gmail.com>
2026-08-18 17:50:14 +0300

    Tracking Performans Optimizasyonu

--- Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -15,13 +15,13 @@ namespace ETicaretAPI.Persistence
     {
         public static void AddPersistenceServices(this IServiceCollection services)
         {
-            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString),ServiceLifetime.Singleton);
-            services.AddSingleton<ICustomerReadRepository,CustomerReadRepository>();
-            services.AddSingleton<ICustomerWriteRepository,CustomerWriteRepository>();
-            services.AddSingleton<IProductWriteRepository,ProductWriteRepository>();
-            services.AddSingleton<IProductReadRepository,ProductReadRepository>();
-            services.AddSingleton<IOrderReadRepository,OrderReadRepository>();
-            services.AddSingleton<IOrderWriteRepository,OrderWriteRepository>();
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+            services.AddScoped<ICustomerReadRepository,CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository,CustomerWriteRepository>();
+            services.AddScoped<IProductWriteRepository,ProductWriteRepository>();
+            services.AddScoped<IProductReadRepository,ProductReadRepository>();
+            services.AddScoped<IOrderReadRepository,OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository,OrderWriteRepository>();
 
         }
         
--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,15 +18,26 @@ namespace ETicaretAPI.API.Controllers
             _productWriteRepository = productWriteRepository;
         }
         [HttpGet]
-        public async void Get()
+        public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new() {Id = Guid.NewGuid(),Name = "Product1", Price = 100, CreatedDate = DateTime.UtcNow,Stock=10},
-                new() {Id = Guid.NewGuid(),Name = "Product2", Price = 200, CreatedDate = DateTime.UtcNow,Stock=20},
-                new() {Id = Guid.NewGuid(),Name = "Product3", Price = 300, CreatedDate = DateTime.UtcNow,Stock=30},
-            });
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new() {Id = Guid.NewGuid(),Name = "Product1", Price = 100, CreatedDate = DateTime.UtcNow,Stock=10},
+            //    new() {Id = Guid.NewGuid(),Name = "Product2", Price = 200, CreatedDate = DateTime.UtcNow,Stock=20},
+            //    new() {Id = Guid.NewGuid(),Name = "Product3", Price = 300, CreatedDate = DateTime.UtcNow,Stock=30},
+            //});
+            //await _productWriteRepository.SaveAsync();
+
+            Product p = await _productReadRepository.GetByIdAsync("36767db2-b657-4da6-8d5a-18564db4a67b",false);
+            p.Name = "Sercan";
             await _productWriteRepository.SaveAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(id);
+            return Ok(product);
         }
     }
 }
